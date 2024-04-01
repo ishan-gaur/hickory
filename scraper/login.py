8829897753fffc5be558a4433c7158d8d355001c
nirmalb9 <nbalachundhar@gmail.com>
2024-03-31 17:43:58 -0700

    initial login flow

--- scraper/login.py
+++ scraper/login.py
@@ -1,14 +1,41 @@
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+
+from dotenv import load_dotenv
+import os
+
+import time
+
+import logging
+
+load_dotenv()
 
 browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-browser.implicitly_wait(5)
+# browser.implicitly_wait(5)
 
 browser.get("https://www.facebook.com/login")
 
-email_input = browser.find_element_by_id("email")
-print(f"email: {email_input}")
-password_input = browser.find_element_by_id("pass")
+time.sleep(1)
 
-browser.implicitly_wait(5)
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+
+time.sleep(1)
+
+username_input = browser.find_element(By.ID, "email")
+password_input = browser.find_element(By.ID, "pass")
+
+username_input.send_keys(username)
+password_input.send_keys(password)
+
+time.sleep(1)
+
+login_button = browser.find_element(By.NAME, "login")
+login_button.click()
+
+time.sleep(30)
+
+# Now logged in 
+
