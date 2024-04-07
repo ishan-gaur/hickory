060ffa0b86f5656c34831e8bef41b822fa6466c1
Ishan Gaur <ishangaur4@gmail.com>
2024-04-07 11:25:47 -0700

    update to use the recommended tags, take tool description out of system prompt, add scraper and write initial code for tool execution

--- claude_api.py
+++ claude_api.py
@@ -1,36 +1,52 @@
-import anthropic
-import pprint
+import os
+import re
+import io
+import sys
 import code
+import pprint
+import logging
 from typing import Dict, List
 from dotenv import load_dotenv
-import logging
-import re
+
+import anthropic
+from scraper import FacebookScraper
+from playwright.sync_api import sync_playwright
+
 load_dotenv()
 
 
-client = anthropic.Anthropic()
-
+client = anthropic.Anthropic(
+    api_key=os.environ.get("ANTHROPIC_KEY")
+)
 
 SYSTEM_PROMPT = """
 You are a shopping assistant named Hickory, helping a user find suitable items on Facebook marketplace.
 
 Don't be verbose and only ask the user one question at a time. The user is on a mobile platform and will find
 it annoying to read or type out large amounts of text. Your objective is to help them make a good purchase and save them time.
-Make sure to do your own due diligence considering their preferences before suggesting any items to buy.
 
-Keep in mind that you are a long-term assistant. If the request is urgent, by all means come back to them with actually buying options. 
-But you can also guide them by monitoring the state of the marketplace over a few weeks even, if they have time. You are also a permanent 
-assistant that will help them over time with multiple purchases; this is not a one-time interaction.
+Answer the user's request using relevant tools (if they are available). Before calling a tool, do some analysis within <thinking></thinking> tags. 
+First, think about which of the provided tools is the relevant tool to answer the user's request. 
+Second, go through each of the required parameters of the relevant tool and determine if the user has directly provided or given enough information to infer a value. 
+When deciding if the parameter can be inferred, carefully consider all the context to see if it supports a specific value. 
+If all of the required parameters are present or can be reasonably inferred, close the thinking tag and proceed with the tool call.
+BUT, if one of the values for a required parameter is missing, DO NOT invoke the function (not even with fillers for the missing params) and instead, 
+ask the user to provide the missing parameters. DO NOT ask for more information on optional parameters if it is not provided.
 
-Note that the user won't see any text wrapped in tags surrounded by triple backticks. You can use this feature in two ways:
-You can also use the ```THINKING``` tag if you would like scratch space to deliberate that the user shouldn't see. 
-You can also display an image by wrapping the URL in ```IMAGE``` tags.
+You can also display an image by wrapping the URL in <image></image> tags.
 
 Feel free to make multiple calls to tools or to tell the user you will go deliberate or take a few days to explore what comes on and off
 the market before going back to the user with a suggestion.
 
-Start off by introducing yourself to the user. The first user message will be a ```PLACEHOLDER``` and they won't see that. Your message will be the first thing they see.
+Make sure to do your own due diligence considering their preferences before suggesting any items to buy. To this end, make sure
+to do analysis within <thinking></thinking> tags to brainstorm possible user preferences that might be relevant, especially after you get new
+information from the user or a tool.
 
+Keep in mind that you are a long-term assistant that will help them over time with multiple purchases; this is not a one-time interaction. 
+If the request is urgent, by all means come back to them with buying options immediately. 
+However, if they have the time, you can also guide them by monitoring the state of the marketplace over a few days, or weeks even.
+
+Start off by introducing yourself to the user. The first user message will be a ```PLACEHOLDER``` and they won't see that. Your message will be the first thing they see.
 """
 
 TOOLS_SPECIFICATION = [
@@ -41,7 +57,7 @@ TOOLS_SPECIFICATION = [
             "type": "object",
             "properties": {
                 "city": {
-                    "type": "string",
+                    "type": "string", # TODO: Change to enum
                     "description": "Which city to search for the item in"
                 },
                 "query": {
@@ -69,7 +85,7 @@ TOOLS_SPECIFICATION = [
             },
             "required": "listing_id"
         }
-    }
+    },
     {
         "name": "get_image",
         "description": "Downloads cover image for a listing and includes it in the next message, for you to view",
@@ -89,70 +105,76 @@ TOOLS_SPECIFICATION = [
 
 
 class ClaudeClient:
-    def __init__(self):
+    def __init__(self, playwright_context) -> None:
         self.client = client
-        self.user_messages = [
-            {"role": "user", "content": "```PLACEHOLDER```"}
+        self.messages = [
+            {
+                "role": "user",
+                "content": "```PLACEHOLDER```"
+            }
         ]
-        self.user_messages_with_tools = self.user_messages
+        self.scraper = FacebookScraper(playwright_context)
+        self.scraper.login()
+        self.interpreter = code.InteractiveInterpreter(locals={
+            'scraper': self.scraper,
+            'pp': pprint.PrettyPrinter(indent=4)
+        })
 
     def get_claude_message(self) -> str:
         message = client.beta.tools.messages.create(
             model="claude-3-opus-20240229",
-            max_tokens=1000,
+            max_tokens=2048,
             tools=TOOLS_SPECIFICATION,
             temperature=0.0,
             system=SYSTEM_PROMPT,
-            messages=self.user_messages
+            messages=self.messages
         )
 
         return message.content
 
-    def is_tool_used(self, message_content: Dict) -> bool:
+    # Need to make sure that the model eventually says something back to the user
+    def has_text_output(self, message_content: Dict) -> bool:
         content = message_content["content"]
+        has_text = False
         for block in content:
-            if block["type"] == "tool_use":
-                return True
-        return False
+            if block["type"] == "text":
+                has_text = True
+                break
+        return has_text
     
-    def call_tool(self, message_content: Dict):
+    def get_tool_responses(self, message_content: Dict) -> List[Dict]:
         content = message_content["content"]
-        method_name, args = None, None
+        tool_responses = []
         for block in content:
             if block["type"] == "tool_use":
                 method_name = block["name"]
                 args = block["input"]
                 tool_use_id = block["id"]
-                break
-        
-        if method_name is None:
-            logging.info(f"No tool use was found...")
-            return
 
-        # TODO: Call method and argument set from the scraper
-        response = method_name(args)
-        
-        self.user_messages_with_tools.append(
-            {
-                "role": "user",
-                "content": [
+                args_str = ", ".join([f"{k}={v}" for k, v in args.items()])
+                method_call = f"{method_name}({args_str})"
+
+                # Setup to capture stdout
+                old_stdout = sys.stdout
+                sys.stdout = io.TextIOWrapper(io.BytesIO(), sys.stdout.encoding)
+
+                # Execute method and read printed output
+                self.interpreter.runsource(f"pp.pprint(scraper.{method_call})")
+                sys.stdout.seek(0)
+                response = sys.stdout.read()
+
+                # Restore stdout.
+                sys.stdout.close()
+                sys.stdout = old_stdout
+
+                tool_responses.append(
                     {
                         "type": "tool_result",
                         "tool_use_id": tool_use_id,
                         "content": response
                     }
-                ]
-            }
-        )
-        message = client.beta.tools.messages.create(
-            model="claude-3-opus-20240229",
-            max_tokens=1000,
-            tools=TOOLS_SPECIFICATION,
-            temperature=0.0,
-            system=SYSTEM_PROMPT,
-            messages=self.user_messages_with_tools
-        )
-        return message.content
+                )
+        return tool_responses
 
     def format_response(self, content_blocks: List[Dict]):
         last_content_block = content_blocks[-1].text
@@ -166,7 +188,7 @@ class ClaudeClient:
         new_user_message =  {"role": "user", "content": user_message}
 
         # Add new user message to both queues
-        self.user_messages.append(new_user_message)
+        self.messages.append(new_user_message)
         self.user_messages_with_tools.append(new_user_message)
 
         response = self.get_claude_message()
@@ -187,16 +209,14 @@ class ClaudeClient:
 
 if __name__ == "__main__":
     pp = pprint.PrettyPrinter(indent=4)
-    claude_client = ClaudeClient()
-    output = None
-    interpreter = code.InteractiveInterpreter(locals={
-        'claude_client': claude_client,
-        'pp': pp
-    })
-    initial_message = claude_client.get_claude_message()
-    claude_client.format_response(initial_message)
-    # pp.pprint(cleaned_message)
-    msg = input("> ")
-    while msg != "exit()":
-        interpreter.runsource(claude_client.read_message({msg}))
-        cmd = input("> ")
+    with sync_playwright() as p:
+        claude_client = ClaudeClient(p)
+        pp.pprint(initial_message = claude_client.get_claude_message())
+        input()
+        pp.pprint(claude_client.messages)
+    # claude_client.format_response(initial_message)
+    # # pp.pprint(cleaned_message)
+    # msg = input("> ")
+    # while msg != "exit()":
+    #     interpreter.runsource(claude_client.read_message({msg}))
+    #     cmd = input("> ")
